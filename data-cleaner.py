590f51ee8120e0b964e1e33da4bd535ec699be30
OneHandKlap <45212899+OneHandKlap@users.noreply.github.com>
2019-12-14 12:42:54 -0500

    Changes made to date-time formatting

--- data-cleaner.py
+++ data-cleaner.py
@@ -18,13 +18,25 @@ def simplify(item):
 os.chdir("C:/Users/pabou/Documents/GitHub/Web-Scraper/data")
 for file in os.listdir("C:/Users/pabou/Documents/GitHub/Web-Scraper/data"):
     if file.endswith(".csv"):
-        header = {"Date":datetime.date,"Time":datetime.time,"Type":str,"Fee":str,"Amount":float,"Status":str}
+        print("Processing: "+file)
+        header = {"Date":str,"Time":str,"Type":str,"Fee":str,"Amount":float,"Status":str}
         data = pd.read_csv(file, sep='delimiter',delimiter=",",header=None,engine='python',error_bad_lines=False)
         data.dropna(how="all",inplace=True)
         data.drop([2,3,5,6,9], axis=1, inplace=True)
         data.dropna(how="any",inplace=True)
         data.columns=header
+        print("Formatting date-time")
         data['Fee'].apply(simplify)
         data['Amount'].apply(simplify)
+        data.columns=header
+        datetime=data["Date"].map(str)+" "+data["Time"]
+        data.insert(3,column="Date-Time",value=datetime)
+
+        data["Time"]=pd.to_datetime(data["Time"])
+        data['Time'] = data['Time'].dt.time
+        data["Date"]=pd.to_datetime(data["Date"])
+
+        data["Date-Time"]=pd.to_datetime(data["Date-Time"])
         data.to_csv("Cleaned_"+file)
+
 
