757eb295cb09b6da556b62c22b6bf3daa42492a0
OneHandKlap <45212899+OneHandKlap@users.noreply.github.com>
2019-12-16 12:14:05 -0500

    weekly-avg jupyter notebook template

--- data-cleaner2.py
+++ data-cleaner2.py
@@ -4,15 +4,15 @@ import os
 import matplotlib as plt
 
 os.chdir("C:/Users/pabou/Documents/Github/Web-Scraper/data/cleaned")
-for file in os.listdir("C:/Users/pabou/Documents/GitHub/Web-Scraper/data"):
+for file in os.listdir("C:/Users/pabou/Documents/GitHub/Web-Scraper/data/cleaned"):
     if file.endswith(".csv"):
 
-        dataset = pd.read_csv("Cleaned_Depanneur Amigos Gatineau.csv")
+        dataset = pd.read_csv(file)
         print(dataset.dtypes)
         dataset["Date-Time"]=pd.to_datetime(dataset["Date-Time"])
         print(dataset.dtypes)
-        dataset["Week"]=dataset['Date-Time'].apply(lambda x: str(x.isocalendar()[0]) + '-' + str(x.isocalendar()[1]).zfill(2))
-        dataset["Weekday"]=dataset['Date-Time'].apply(lambda x: str(x.isocalendar()[0]) + '-' + str(x.isocalendar()[2]).zfill(2))
+        dataset["Week"]=dataset['Date-Time'].apply(lambda x: str(x.isocalendar()[1]).zfill(2))
+        dataset["Weekday"]=dataset['Date-Time'].apply(lambda x: str(x.isocalendar()[2]).zfill(2))
         dataset["Month"]=pd.DatetimeIndex(dataset['Date-Time']).month
         print(dataset.head())
         dataset.to_csv("Cleaned2_"+file)
